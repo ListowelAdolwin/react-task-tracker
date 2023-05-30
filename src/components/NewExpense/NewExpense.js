3fbe4bb990dc2d74634c464de57113960e8d60cd
ListowelAdolwin <listoweladolwin@gmail.com>
2023-05-30 07:10:30 +0000

    Handled the form submit

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import ExpenseForm from './ExpenseForm';
-import './NewExpense.css';
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+  const expenseDataHandler = (inputData) => {
+    const expenseData = {
+      ...inputData,
+      id: Math.random().toString(),
+    };
+    console.log("Finally");
+    console.log(expenseData);
+    props.onAddExpense(expenseData);
+  };
+
   return (
-    <div className='new-expense'>
-      <ExpenseForm />
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={expenseDataHandler} />
     </div>
   );
 };

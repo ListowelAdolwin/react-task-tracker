860abab3ea571c7a72b9a61b6e0521e6950c066a
ListowelAdolwin <listoweladolwin@gmail.com>
2023-06-01 13:52:59 +0000

    Implemented the filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,36 +6,35 @@ import { useState } from "react";
 
 function Expenses(props) {
   const expenses = props.expenses;
-  const [year, setYear] = useState("");
+  const [year, setYear] = useState("all");
   const onChaneYearHandler = (year) => {
     setYear(year);
   };
+
+  let filteredExpenses = expenses
+
+
+  if (year !== 'all') {
+    filteredExpenses = expenses.filter(
+      (expense) => expense.date.getFullYear().toString() === year
+    );
+  }
+
+
   return (
     <div>
       <Card className="expenses">
-        <ExpenseFilter onChangeYear={onChaneYearHandler} />
-        <ExpenseItem
-          title={expenses[0].title}
-          amount={expenses[0].amount}
-          date={expenses[0].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={expenses[1].title}
-          amount={expenses[1].amount}
-          date={expenses[1].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          date={expenses[2].date}
-          title={expenses[2].title}
-          amount={expenses[2].amount}
-        ></ExpenseItem>
-        <ExpenseItem
-          date={expenses[3].date}
-          title={expenses[3].title}
-          amount={expenses[3].amount}
-        ></ExpenseItem>
-        Year:
-        {year}
+        <ExpenseFilter selected={year} onChangeYear={onChaneYearHandler} />
+        {filteredExpenses.length === 0 && <p className="expenses-list__fallback"> No items found</p>}
+        {filteredExpenses.length > 0 &&
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              date={expense.date}
+              amount={expense.amount}
+            />
+          ))}
       </Card>
     </div>
   );
